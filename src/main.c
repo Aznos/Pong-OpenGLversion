5405cc9eef7ad8cf2d056ef11c7168ae624ff976
Pigioty <Pigiotyreal@gmail.com>
2024-01-31 12:26:39 -0500

    set up winmain

--- src/main.c
+++ src/main.c
@@ -6,3 +6,12 @@ LRESULT CALLBACK WindowProcedure(HWND, UINT, WPARAM, LPARAM);
 
 void EnableOpenGL(HWND hwnd, HDC*, HGLRC*);
 void DisableOpenGL(HWND, HDC, HGLRC);
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) {
+    WNDCLASS wc;
+    HWND hwnd;
+    HDC hdc;
+    HGLRC hrc;
+    MSG msg;
+    BOOL bQuit = FALSE;
+}
