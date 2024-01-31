d41321d43bb5c6c2b9846fc8f46dc3f9f1beb97d
Pigioty <Pigiotyreal@gmail.com>
2024-01-31 12:25:38 -0500

    here we go

--- src/main.c
+++ src/main.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
+#include <windows.h>
+#include <GL/gl.h>
 
-int main(int argc, char *argv[]) {
-    printf("Hello, world!\n");
-    return 0;
-}
+LRESULT CALLBACK WindowProcedure(HWND, UINT, WPARAM, LPARAM);
+
+void EnableOpenGL(HWND hwnd, HDC*, HGLRC*);
+void DisableOpenGL(HWND, HDC, HGLRC);
